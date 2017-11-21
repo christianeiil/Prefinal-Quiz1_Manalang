33182844358283b8d9db6ecfbbf16b2f716aceb6
christianeiil <christianeiil14@gmail.com>
2017-11-22 00:45:41 +0800

    Prefinal-Quiz1_Manalang

---
+++ PreFinalQuiz1_Manalang/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PreFinalQuiz1_Manalang.Startup))]
+namespace PreFinalQuiz1_Manalang
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
